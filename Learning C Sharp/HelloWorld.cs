0105260f3147accee6b8b0ae540fbf8eb219dd7a
Jamel <Jamel@HELIOSPHERE>
2026-03-18 16:13:58 +0900

    Add project files.

---
+++ Learning C Sharp/HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    // Public classes allows developers to access them from other locations.
+    public class HelloWorld
+    {
+        int a = 1;
+        int b = 2;
+
+        // Public method. Notice PascalCase convention for methods and classes.
+        // A static local function can't capture local variables or instance state.
+        public static void Print()
+        {
+            Console.WriteLine("Hello World!");
+            // Notice that you cannot access a or b variables here. That's because this is a static method. Local variables only.
+        }
+
+        // Private method. Only accessible here.
+        private void PrivatePrint()
+        {
+            // We can access class variables here because we're not a static method.
+            a++;
+            b--;
+            Console.WriteLine("Sheeesh! Hello World!");
+        }
+    }
+}
---
+++ Learning C Sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args,)
+        {
+            // Use camelCase for method arguments, local variables, and private fields.
+            int numberInterger = 0;
+            Console.WriteLine(numberInterger);
+
+            HelloWorld.Print();
+        }
+    }
+}
